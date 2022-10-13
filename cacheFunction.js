cabf7a56a023e3f0458fb7d7246d542e967575a5
Sravan Gunaganti <88744881+SravanGunaganti@users.noreply.github.com>
2022-10-13 13:14:53 +0530

    Version-1

--- cacheFunction.js
+++ cacheFunction.js
@@ -1,13 +1,3 @@
-// const cacheFunction = (cb) => {
-//     const output = {};
-//     return function (arg) {
-//       if (output[arg]) return output[arg];
-//       output[arg] = cb(arg);
-//       return output[arg];
-//     };
-//   };
-
-
 
 function cacheFunction(cb) {
     let cache = {};
